6c193686ed1a844b65369deea08dc43f7607640c
Michael Levan <mlevan1992@gmail.com>
2020-08-02 10:57:51 -0400

    List EC2 info by instance ID

--- EC2/ListInstances/src/main.go
+++ EC2/ListInstances/src/main.go
@@ -1,8 +1,10 @@
+// Example: go run main.go i-0858366964463b4db
+
 package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -10,21 +12,24 @@ import (
 )
 
 func main() {
-	//instanceID := os.Args[1]
-	listInstances()
+	instanceID := os.Args[1]
+	listInstances(instanceID)
 }
 
-func listInstances() {
-	fmt.Println("Listing EC2 Instances...")
-
-	awsConnect := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-2"),
-	})
+func listInstances(instanceID string) {
+	awsConnect, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-2")},
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	ec2sess := ec2.New(awsConnect)
 
-	input := &ec2.DescribeInstancesInput{
-		InstanceIds: []*string{aws.String("i-0858366964463b4db")},
+	instanceInfo := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String(instanceID)},
 	}
-	fmt.Println(ec2sess.DescribeInstances(input))
+	fmt.Println("Listing EC2 Instance info...")
+
+	fmt.Println(ec2sess.DescribeInstances(instanceInfo))
 }
